8af21a97c907df031eeaa915a356c6353f7b1e06
JFabian-Guzman <fabiguzmang1205@gmail.com>
2025-01-21 12:19:18 -0600

    Set up

---
+++ src/config/settings.py
@@ -0,0 +1,11 @@
+import pygame
+
+WINDOW_WIDTH, WINDOW_HEIGHT = 1366, 720
+COLORS = {
+  "Blue": "#1A237E",
+  "Amber": "#FFC107",
+  "GREEN": "#4CAF50",
+  "RED": "#F44336",
+  "WHITE": "#FFFFFF",
+  "BLACK": "#000000"
+}
--- src/main.py
+++ src/main.py
@@ -1,1 +1,22 @@
-print("Hello world")
+from config.settings import *
+
+class Game: 
+  def __init__(self):
+    pygame.init()
+    self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.display.set_caption("Who wants to be a millionaire?")
+    self.clock = pygame.time.Clock()
+    self.running = True
+  
+  def run(self):
+    while self.running:
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          self.running = False
+      self.screen.fill((0, 0, 0))
+      pygame.display.update()
+    pygame.quit()
+
+if __name__ == "__main__":
+  game = Game()
+  game.run()
