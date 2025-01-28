b123d34914de525799ab5b82face13e6f154e783
JFabian-Guzman <fabiguzmang1205@gmail.com>
2025-01-28 10:54:16 -0600

    Small Improvements

--- src/Game.py
+++ src/Game.py
@@ -27,16 +27,12 @@ class Game:
     self.overlay = pygame.Surface(self.screen.get_size())
     self.overlay.fill((0, 0, 0))
     self.overlay.set_alpha(128) # Transparency level
-    banner = pygame.Surface(BANNER["SIZE"])
-    self.draw_banner(banner)
-
-
-
-    self.screen.blit(banner, (0, WINDOW_HEIGHT - 70))
+    self.banner = pygame.Surface(BANNER["SIZE"])
+    self.banner_rect = self.banner.get_rect(bottom = WINDOW_HEIGHT)
     
   
-  def draw_banner(self, banner):
-    banner.fill(COLORS["WHITE"])
+  def draw_banner(self):
+    self.banner.fill(COLORS["WHITE"])
     # logos
     ucr_logo = pygame.image.load(join("assets", "img", "ucr_logo.png")).convert_alpha()
     ucr_logo = pygame.transform.scale(ucr_logo, (300, 60))
@@ -48,9 +44,16 @@ class Game:
     tcu_logo = pygame.transform.scale(tcu_logo, (30, 60))
     tcu_rect = tcu_logo.get_rect(center = (WINDOW_WIDTH - 100, BANNER["SIZE"][1] // 2))
 
-    banner.blit(ucr_logo, ucr_rect)
-    banner.blit(elm_logo, elm_rect)
-    banner.blit(tcu_logo, tcu_rect)
+    self.banner.blit(ucr_logo, ucr_rect)
+    self.banner.blit(elm_logo, elm_rect)
+    self.banner.blit(tcu_logo, tcu_rect)
+    self.screen.blit(self.banner, self.banner_rect)
+
+  def draw_background(self):
+    self.screen.blit(self.background, (0, 0))
+    self.screen.blit(self.overlay, (0, 0))
+    self.draw_banner()
+
 
   def run(self):
     while self.running:
@@ -61,12 +64,8 @@ class Game:
         if event.type == pygame.QUIT:
           self.running = False
       #draw
-      self.screen.blit(self.background, (0,0))
-      self.screen.blit(self.overlay, (0, 0)) # Reduce the brightness of the background
-      # self.menu.draw()
-      # self.cursor.draw()
-      # self.cursor.update()
-      # self.menu.update()
+      self.draw_background()
+      
       self.global_sprites.update()
 
       self.menu.draw()
