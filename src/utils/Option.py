09807ef2923cf0a39614fdbefae8ee5a05eaf781
JFabian-Guzman <fabiguzmang1205@gmail.com>
2025-02-11 15:54:42 -0600

    Fix questions and answer limits

--- src/utils/Option.py
+++ src/utils/Option.py
@@ -8,10 +8,20 @@ class Option(pygame.sprite.Sprite):
     self.image = pygame.image.load(join("assets", "img" ,"option.png")).convert_alpha()
     self.rect = self.image.get_rect(center = position)
     self.screen = pygame.display.get_surface()
-    self.font = pygame.font.Font(join("assets", "fonts", "PressStart2P-Regular.ttf"), 20)
+    self.font = pygame.font.Font(join("assets", "fonts", "PressStart2P-Regular.ttf"), 14)
     self.title = text
-    self.text = self.font.render(text, True, COLORS["WHITE"])
+    self.display_text = self.wrap_text(text)
+    self.text = self.font.render(self.display_text, True, COLORS["WHITE"])
     self.text_rect = self.text.get_rect(center = self.rect.center)
+
+  def wrap_text(self, text):
+        if len(text) > 24:
+            mid = len(text) // 2
+            if ord(text[mid]) >= 65 and ord(text[mid]) <= 90 or ord(text[mid]) >= 97 and ord(text[mid]) <= 122:
+              return text[:mid] + "-\n" + text[mid:]
+            else:
+              return text[:mid] + "\n" + text[mid:]
+        return text
 
   def update(self):
     self.screen.blit(self.text, self.text_rect)
@@ -24,7 +34,8 @@ class Option(pygame.sprite.Sprite):
 
   def set_title(self,text):
     self.title = text
-    self.text = self.font.render(text, True, COLORS["WHITE"])
+    self.display_text = self.wrap_text(text)
+    self.text = self.font.render(self.display_text, True, COLORS["WHITE"])
     self.text_rect = self.text.get_rect(center = self.rect.center)
 
 
--- src/utils/Questions.py
+++ src/utils/Questions.py
@@ -9,7 +9,7 @@ class Question(pygame.sprite.Sprite):
     self.image = pygame.image.load(join("assets", "img" ,"question.png")).convert_alpha()
     self.rect = self.image.get_rect(center = (WINDOW_WIDTH/2,200))
     self.screen = pygame.display.get_surface()
-    self.font = pygame.font.Font(join("assets", "fonts", "PressStart2P-Regular.ttf"), 20)
+    self.font = pygame.font.Font(join("assets", "fonts", "PressStart2P-Regular.ttf"), 14)
     self.event_manager = event_manager
     self.title = ''
     self.text = self.font.render(self.title, True, COLORS["WHITE"])
@@ -23,6 +23,15 @@ class Question(pygame.sprite.Sprite):
     self.text = self.font.render(self.title, True, COLORS["WHITE"])
     self.text_rect = self.text.get_rect(center = self.rect.center)
 
+  def wrap_text(self, text):
+    if len(text) > 54:
+        mid = len(text) // 2
+        if ord(text[mid]) >= 65 and ord(text[mid]) <= 90 or ord(text[mid]) >= 97 and ord(text[mid]) <= 122:
+          return text[:mid] + "-\n" + text[mid:]
+        else:
+          return text[:mid] + "\n" + text[mid:]
+    return text
+
   def get_rect(self):
     return self.rect
 
@@ -30,10 +39,11 @@ class Question(pygame.sprite.Sprite):
     return self.title
   
   def change_question(self, *args):
-    self.title = args[0]
+    self.title = self.wrap_text(args[0])
+    # self.title = args[0]
 
   def set_up_question_events(self):
     self.event_manager.subscribe("change_question", self.change_question)
 
-
+ 
 
